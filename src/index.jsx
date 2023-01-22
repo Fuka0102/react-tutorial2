414a7a206ac0d95a97f23d520afaf022d1ad79c6
zumi0102 <tanakafuka0102@gmail.com>
2023-01-22 13:46:27 +0900

    ベースのソースを用意

--- src/index.jsx
+++ src/index.jsx
@@ -4,10 +4,10 @@ import ReactDOM from 'react-dom/client';
 // import { App } from './displayGif2';
 // import { App } from './form';
 // import { App } from './passwordForm';
-import { NumberGuessing } from './guessNumber';
+// import { NumberGuessing } from './guessNumber';
+import { App } from './todo';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 // root.render(app);
 // root.render(list);
-// root.render(<App />);
-root.render(<NumberGuessing />);
+root.render(<App />);
