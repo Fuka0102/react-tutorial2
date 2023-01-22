407c64e33ee0c1997c7fe00830a5a8046ff73a41
zumi0102 <tanakafuka0102@gmail.com>
2023-01-22 14:06:50 +0900

    todoのテストデータ作成

--- src/todo.jsx
+++ src/todo.jsx
@@ -3,6 +3,11 @@ import React from 'react';
 const getKey = () => Math.random().toString(32).substring(2);
 
 function Todo() {
+  const [items, setItems] = React.useState([
+    { key: getKey(), text: 'Learn JavaScript', done: false },
+    { key: getKey(), text: 'Learn React', done: false },
+    { key: getKey(), text: 'Get some good sleep', done: false },
+  ]);
   return null;
 }
 
